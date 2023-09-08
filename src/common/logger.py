08126c76666601ef94e390fa8643dea8c1a3c5cf
Soyeon Hwang <thdus0619@gmail.com>
2023-09-08 01:36:59 +0000

    Modified train.py

--- src/common/logger.py
+++ src/common/logger.py
@@ -29,14 +29,14 @@ def get_file_handler(
     Returns:
         logging.handlers.TimedRotatingFileHandler: 로그 저장 파일 핸들러 객체
     """
-    file_handler = logging.handlers.TimedRotatingFileHandler(
+    file_handler = logging.handlers.TimedRotatingFileHandler(  # 어떤 기간을 두고 그 전의 로그는 필요없을 때 사용할 수 있음
         log_path, when="midnight", interval=1, backupCount=30, encoding="utf-8"
     )
     file_handler.suffix = "logs-%Y%m%d"
-    # TODO: 파일 핸들러의 기본 수준을 INFO로 설정
-    
-    # TODO: 파일 핸들러의 포맷을 FILE_HANDLER_FORMAT으로 설정
-    
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(
+        logging.Formatter(FILE_HANDLER_FORMAT)
+    )  # file_handler 만드는 기본
     return file_handler
 
 
@@ -56,11 +56,10 @@ def set_logger(log_path: str = LOG_FILEPATH) -> logging.Logger:
     )
 
     logger = logging.getLogger("rich")
-    
-    # TODO: 로거의 기본 수준을 DEBUG 설정
-    
-    # TODO: 기본 로거에 위에서 만든 파일 핸들러를 추가
-    
+
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(get_file_handler(log_path=log_path))
+
     return logger
 
 
